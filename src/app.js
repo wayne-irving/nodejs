52ba6ab1a7cc4be3a050eee86677f36ac0525fb3
Wayne Irving <wayne.patrick.irving@gmail.com>
2024-01-08 06:10:44 -0600

    json

--- src/app.js
+++ src/app.js
@@ -2,8 +2,33 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const json = {
+    "name": "Wayne Irving",
+    "industry": "IT",
+    "favoriteColors": [
+        "red",
+        "orange",
+        "black"
+    ],
+    "favoriteNumbers": [
+        5,
+        3,
+        7
+    ],
+    "favoritePeople": [
+        {
+            "name": "Jarrid",
+            "relationship": "friend"
+        },
+        {
+            "name": "granny",
+            "relationship": "grandparent"
+        }
+    ]
+};
+
 app.get('/', (req, res) => {
-    res.send('hello world!');
+    res.send({"data": json});
 })
 
 app.post('/', (req, res) => {
